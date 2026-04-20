38f6e51a7e52ae5fc4a20d4bcbae071108d497c6
SLY-MJ <3144920368@qq.com>
2026-04-20 10:56:45 +0800

    feat(BaseController):启用反射类

---
+++ src/main/java/controller/BaseController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class BaseController extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri=req.getRequestURI();
+
+        int index=uri.lastIndexOf("/");
+
+        String methodName=uri.substring(index+1);
+
+        Class<? extends BaseController> cls=this.getClass();
+
+        try {
+            Method method= cls.getMethod(methodName,HttpServletRequest.class,HttpServletResponse.class);
+            method.invoke(this,req,resp);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
