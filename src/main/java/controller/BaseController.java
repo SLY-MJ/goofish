d3a94df74d9bb52a665439ed0bac2955fdcbbb6d
SLY-MJ <3144920368@qq.com>
2026-04-20 12:24:37 +0800

    refactor:让Base实现接口JsonUtil 删除其余controller的imp来是代码简介

--- src/main/java/controller/BaseController.java
+++ src/main/java/controller/BaseController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import util.JsonUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +10,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class BaseController extends HttpServlet {
+public class BaseController extends HttpServlet implements JsonUtil {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String uri=req.getRequestURI();
