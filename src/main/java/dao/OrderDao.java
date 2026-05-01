1fbcef40d26b6719e325dff262320b192ee541b4
SLY-MJ <3144920368@qq.com>
2026-05-01 17:36:23 +0800

    feat(OrderDao):增加了订单的数量的添加和更改

--- src/main/java/dao/OrderDao.java
+++ src/main/java/dao/OrderDao.java
@@ -9,18 +9,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OrderDao {
-    public OrderDao() {
-    }
 
-    public long add(long itemId,long buyerId,long sellerId,double amount ,OrderStatus status) throws SQLException {
-        String sql = "insert into orders(item_id,buyer_id,seller_id,amount,status) values(?,?,?,?,?)";
+    public long add(long itemId,long buyerId,long sellerId,int number,double amount ,OrderStatus status) throws SQLException {
+        String sql = "insert into orders(item_id,buyer_id,seller_id,number,amount,status) values(?,?,?,?,?,?)";
         try (Connection c = DbUtil.getConnection()) {
             PreparedStatement ps = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setLong(1, itemId);
             ps.setLong(2, buyerId);
             ps.setLong(3, sellerId);
-            ps.setDouble(4, amount);
-            ps.setString(5, status.name());
+            ps.setInt(4, number);
+            ps.setDouble(5, amount);
+            ps.setString(6, status.name());
             ps.executeUpdate();
 
             ResultSet rs = ps.getGeneratedKeys();
@@ -31,13 +30,12 @@ public class OrderDao {
         return -1;
     }
 
-    public boolean delete(long id) throws SQLException {
+    public void delete(long id) throws SQLException {
         String sql = "delete from orders where id=?";
         try (Connection c = DbUtil.getConnection()) {
             PreparedStatement ps = c.prepareStatement(sql);
             ps.setLong(1, id);
             ps.executeUpdate();
-            return true;
         }
     }
 
@@ -113,14 +111,23 @@ public class OrderDao {
 
 
 
-    public boolean updateStatus(long id, OrderStatus status) throws SQLException {
+    public void updateStatus(long id, OrderStatus status) throws SQLException {
         String sql = "update orders set status=? where id=?";
         try (Connection c = DbUtil.getConnection()) {
             PreparedStatement ps = c.prepareStatement(sql);
             ps.setString(1, status.name());
             ps.setLong(2, id);
             ps.executeUpdate();
-            return true;
+        }
+    }
+
+    public void updateNumber(long id, int number) throws SQLException {
+        String sql = "update orders set number=? where id=?";
+        try (Connection c = DbUtil.getConnection()) {
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setInt(1, number);
+            ps.setLong(2, id);
+            ps.executeUpdate();
         }
     }
 
@@ -130,6 +137,7 @@ public class OrderDao {
                 rs.getLong("item_id"),
                 rs.getLong("buyer_id"),
                 rs.getLong("seller_id"),
+                rs.getInt("number"),
                 rs.getDouble("amount"),
                 OrderStatus.valueOf(rs.getString("status")),
                 rs.getString("created_at")
