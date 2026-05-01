a6a0761ea01ee4daf666ea6ac4819c855718118b
SLY-MJ <3144920368@qq.com>
2026-05-01 17:16:29 +0800

    fix(appFilter):将后端端口改为15001

--- src/main/java/filter/AppFilter.java
+++ src/main/java/filter/AppFilter.java
@@ -26,10 +26,8 @@ public class AppFilter implements Filter, JsonUtil {
             "http://127.0.0.1:3000",
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            "http://localhost:8080",
-            "http://127.0.0.1:8080",
-            "http://localhost:8081",
-            "http://127.0.0.1:8081"
+            "http://localhost:15001",
+            "http://127.0.0.1:15001"
     );
 
     @Override
